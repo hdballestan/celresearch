28aa955d6105b8458d7d3a7f7724fb959afe7c36
hever.ballesta <hever.ballesta@zinobe.com>
2023-02-05 21:37:43 -0500

    emergency update

---
+++ celresearch/foundation/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+
+    body =forms.CharField(
+        label='',
+        widget=forms.Textarea(attrs={
+            'rows': '3',
+            'placeholder': 'Put anything here...'
+        })   
+    )
+
+    class Meta:
+        model = Post
+        fields = ['body']
